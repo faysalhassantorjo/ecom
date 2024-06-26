4ea1878d349eed04327cc523f7fcc163f825a337
faysal <faysalhassantorjo8@gmail.com>
2024-06-26 19:14:26 +0600

    final 5

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,19 +136,11 @@ import os
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-
-
-
-if DEBUG:
-
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-else:
-
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,24 +1,41 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import handler404
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+handler404 = 'shop.views.custom_404_view'
+def handle_unexpected_url(request):
+    return render(request,'shop/404.html')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
 ]
 
 if settings.DEBUG:
-    # Serve media files in development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    # Serve static files in development
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-else:
-    # Serve media files in production (optional, usually handled by web server)
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Note: static files should not be served by Django in production.
+urlpatterns+= staticfiles_urlpatterns()
+
+urlpatterns += [
+    re_path(r'^.*$', handle_unexpected_url),
+]
