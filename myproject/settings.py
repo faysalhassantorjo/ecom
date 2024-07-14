6231f85f2a2d8e46105eb6ecd5afa566cf0c2bca
faysal <faysalhassantorjo8@gmail.com>
2024-07-15 00:06:13 +0600

    commit infinity infinity2

--- myproject/settings.py
+++ myproject/settings.py
@@ -88,7 +88,6 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
