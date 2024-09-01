6e44e391041e79e77e57cf603d30bb6f2d7f7050
faysal <faysalhassantorjo8@gmail.com>
2024-09-01 21:09:52 +0600

    daatabase

--- myproject/settings.py
+++ myproject/settings.py
@@ -90,23 +90,23 @@ EMAIL_HOST_PASSWORD = 'bqkm pqfo mlyh hjop'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'fqsXmXhMveDGnDAjJCuGJVKRYIheNvFF',
-#         'HOST': 'monorail.proxy.rlwy.net',
-#         'PORT': '37839',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fqsXmXhMveDGnDAjJCuGJVKRYIheNvFF',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '37839',
+    }
+}
 
 CSRF_TRUSTED_ORIGINS = [
     'https://longg.up.railway.app',
