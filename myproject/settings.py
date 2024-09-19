0eca8d3646fbda3e438a0c86318dbfcf102c5185
faysal <faysalhassantorjo8@gmail.com>
2024-09-19 20:35:30 +0600

    gg

--- myproject/settings.py
+++ myproject/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://longgfashion.com/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
